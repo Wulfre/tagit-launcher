33898fb9ea1d3fff338c86c0cbc1f5dd4beaa538
Wulfre <wulfre@users.noreply.github.com>
2025-05-26 20:39:20 -0400

    add no-update flag

--- launch_other.go
+++ launch_other.go
@@ -14,7 +14,7 @@ func launchTagIt() {
 	execName := "tagit." + runtime.GOOS + ".x86_64"
 	execPath := filepath.Join(cacheDir, execName)
 	if _, err := os.Stat(execPath); err == nil {
-		cmd := exec.Command(execPath)
+		cmd := exec.Command(execPath, "--no-update")
 		cmd.Start()
 		os.Exit(0)
 	}
--- launch_windows.go
+++ launch_windows.go
@@ -14,7 +14,7 @@ func launchTagIt() {
 	execName := "tagit.windows.x86_64.exe"
 	execPath := filepath.Join(cacheDir, execName)
 	if _, err := os.Stat(execPath); err == nil {
-		cmd := exec.Command(execPath)
+		cmd := exec.Command(execPath, "--no-update")
 		// Set Windows-specific process attributes to hide console window
 		cmd.SysProcAttr = &syscall.SysProcAttr{
 			HideWindow: true,
