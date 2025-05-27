1cf4a58bbd320533f8bdb11e0aeae51b4d010b9d
Wulfre <wulfre@users.noreply.github.com>
2025-05-26 21:28:26 -0400

    update build script

--- build.sh
+++ build.sh
@@ -8,18 +8,13 @@ mkdir -p build
 
 GOOS="linux" \
     GOARCH="amd64" \
-    CC="zig cc -target x86_64-linux-gnu \
-        -isystem /usr/include \
-        -L/usr/lib" \
-    CXX="zig c++ -target x86_64-linux-gnu \
-        -isystem /usr/include \
-        -L/usr/lib" \
     go build -trimpath -ldflags="-s -w" -tags "noaudio" \
-    -o "./build/tagit-launcher-linux"
+    -o "./build/tagit-launcher"
 
 GOOS="windows" \
     GOARCH="amd64" \
-    CC="zig cc -target x86_64-windows-gnu" \
-    CXX="zig c++ -target x86_64-windows-gnu" \
+    CGO_ENABLED="1" \
+    CC="x86_64-w64-mingw32-gcc" \
+    CXX="x86_64-w64-mingw32-g++" \
     go build -trimpath -ldflags="-s -w -H=windowsgui" -tags "noaudio" \
     -o "./build/tagit-launcher.exe"
